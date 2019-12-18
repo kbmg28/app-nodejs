7d0a8a2931e27a7c029aa865672290b17e6ab0be
Kevin Gomes <kevin28bruno@gmail.com>
2019-12-18 08:24:29 -0400

    exemplo de requisições GET e POST

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    let obj = req.query;
+    return res.send({message: `${obj.nome} possui ${obj.idade} anos.`})
+})
+
+app.post('/', (req, res) => {
+    return res.send ({message: 'Tudo ok com o método POST!'})
+})
+
 app.listen(3000);
 
 module.exports = app;
