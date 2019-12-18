7ab7c3335a878915c5fe0544cbeffc34d617fe11
Kevin Gomes <kevin28bruno@gmail.com>
2019-12-18 16:10:20 -0400

    conexão com o mongodb

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const express = require('express');
 const app = express();
+const mongoose = require ('mongoose');
+const bodyParser = require('body-parser')
+
+const url =  'mongodb+srv://usuario_admin:12262628@clusterapi-gfmyz.mongodb.net/test?retryWrites=true&w=majority';
+
+var options = {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true};
+
+mongoose.connect(url,options);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('error', (err) =>{
+    console.log('Erro na conexão com o banco de dados: ' + err);
+})
+
+mongoose.connection.on('disconnected', () =>{
+    console.log('Aplicação desconectada com o banco de dados');
+})
+
+mongoose.connection.on('connected', () =>{
+    console.log('Aplicação conectada com o banco de dados');
+})
+
+//BODY PARSER
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json())
 
 const indexRoute = require('./Routes/index')
 const usersRoute = require('./Routes/users')
