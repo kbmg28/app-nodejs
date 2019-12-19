ba3a23f97a2026292cdba2df516d22a82fece827
Kevin Gomes <kevin28bruno@gmail.com>
2019-12-19 16:27:05 -0400

    retorno de rotas com códigos http apropriados

--- Routes/users.js
+++ Routes/users.js
@@ -13,41 +13,41 @@ router.get('/', async (req, res) => {
         const users = await Users.find({});
         return res.send(users);
     } catch (err) {
-        res.send({ error: 'Erro na consulta de usuários!' });
+        return res.status(500).send({ error: 'Erro na consulta de usuários!' });
     }
 })
 
 router.post('/create', async (req, res) => {
     const {email, password} = req.body;
     
-    if(!email || !password) return res.send({ error: 'Dados insuficientes!'});
+    if(!email || !password) return res.status(400).send({ error: 'Dados insuficientes!'});
     
     try {
-        if (await Users.findOne({email})) return res.send({error: 'Usuário já registrado!'});
+        if (await Users.findOne({email})) return res.status(400).send({error: 'Usuário já registrado!'});
 
         const user = await Users.create(req.body);
         user.password = undefined;
         return res.send({user, token: createUserToken(user.id)});
 
     } catch (err) {
-        return res.send(({ error: 'Erro ao buscar usuário!'}));
+        return res.status(500).send(({ error: 'Erro ao buscar usuário!'}));
     }
 });
 
 router.post('/auth', async (req, res) => {
     const {email, password} = req.body;
-    if(!email || !password) return res.send({ error: 'Dados insuficientes!'});
+    if(!email || !password) return res.status(400).send({ error: 'Dados insuficientes!'});
 
     try {
         const user = await Users.findOne({ email }).select('+password');
-        if (!user) return res.send({error: 'Usuário não registrado!'})
+        if (!user) return res.status(400).send({error: 'Usuário não registrado!'})
 
-        if (!await bcrypt.compare(password, user.password)) return res.send({error: 'Erro ao autenticar usuário!'});
+        if (!await bcrypt.compare(password, user.password)) return res.status(401).send({error: 'Erro ao autenticar usuário!'});
 
         user.password = undefined;
         return res.send({user, token: createUserToken(user.id)});
     } catch (err) {
-       return res.send (({error : 'Erro ao buscar usuário!'}));
+       return res.status(500).send (({error : 'Erro ao buscar usuário!'}));
     }
 });
 
