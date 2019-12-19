fe46da9b8131a8166de891e4e94085f9e637ad55
Kevin Gomes <kevin28bruno@gmail.com>
2019-12-19 09:59:29 -0400

    criptografar senha antes de enviar para o banco

--- model/user.js
+++ model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
     email: { type: String, required: true, unique: true, lowercase: true},
@@ -7,4 +8,12 @@ const UserSchema = new Schema({
     created: { type: Date, default: Date.now}
 });
 
+UserSchema.pre('save', function(next){
+    let user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.hash(user.password, 10, (err, encrypted) => {
+        user.password = encrypted;
+        return next();
+    })
+})
 module.exports = mongoose.model('User', UserSchema);
